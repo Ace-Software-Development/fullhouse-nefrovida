91b16f16c2c36f9ba4f90c8faae89e35278ba15e
Carolina Herrera Martínez <A01411547@itesm.mx>
2022-04-25 22:31:44 -0500

    feat: asignidar posiciones por defecto a los botones

--- client/src/components/BtnGuardar.js
+++ client/src/components/BtnGuardar.js
@@ -1,8 +1,10 @@
 
-const BtnGuardar = ({icono = "save", texto = "Guardar", url = "/guardar"}) => {
+const BtnGuardar = ({icono = "save", texto = "Guardar", url = "/guardar", posicion = "right"}) => {
     return(
-      <a className="waves-effect waves-dark btn btn-guardar green darken-1 white-text text-accent-4" href={url}>{texto}<i className="material-icons left">{icono}</i></a>
+      <a className= {"waves-effect waves-dark btn btn-guardar green darken-1 white-text text-accent-4" + posicion} href={url}>{texto}<i className="material-icons left">{icono}</i></a>
+    
     )
 }
+
 
 export default BtnGuardar
