0b794e21b27483f2805b5feb9f16ef1cf85accc5
Manolo Ramírez Pintor <A01706155@itesm.mx>
2022-04-26 17:46:51 -0500

    Feat: Arreglar flechita CSS, añadir submit en botón. Fix: Arreglar el funcionamiento de Datepicker.

--- client/src/components/BtnGuardar.js
+++ client/src/components/BtnGuardar.js
@@ -1,6 +1,6 @@
 const BtnGuardar = ({icono = "save", texto = "Guardar"}) => {
     return(
-      <button className="waves-effect waves-dark btn btn-guardar green darken-1 white-text text-accent-4 right">{texto}<i className="material-icons left">{icono}</i></button>
+      <button type="submit" className="waves-effect waves-dark btn btn-guardar green darken-1 white-text text-accent-4 right">{texto}<i className="material-icons left">{icono}</i></button>
     )
 }
 
--- client/src/components/Datepicker.js
+++ client/src/components/Datepicker.js
@@ -1,7 +1,7 @@
-const Datepicker = ({tamano = "s12 m4", id = "nombre", label = "Nombre", tipo="text"}) => {
+const Datepicker = ({tamano = "s12 m4", id = "nombre", label = "Nombre"}) => {
     return(
       <div className={"input-field col " + tamano}>
-        <input id={id} type={tipo} name={id} className="datepicker z-depth-1"/>
+        <input name={id} id={id} type="date" className="validate z-depth-1"/>
         <label for={id}>{label}</label>
       </div>
     )
