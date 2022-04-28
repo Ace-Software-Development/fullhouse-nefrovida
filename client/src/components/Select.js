1f51c0fe3a1cf40404d615c7adf5a67c694a1428
Manolo Ramírez Pintor <A01706155@itesm.mx>
2022-04-28 01:06:16 -0500

    feat: hacerlo compatible con hooks

--- client/src/components/Select.js
+++ client/src/components/Select.js
@@ -1,14 +1,12 @@
-import React from 'react';
-
-const Select = ({tamano = "s4 m2", id = "nombre", label = "Nombre", arr = ["Hombre", "Mujer"]}) => {
+const Select = ({value, tamano = "s4 m2", id = "nombre", label = "Nombre", arr = ["Hombre", "Mujer"], handleChange}) => {
 
     return(
       <div className={"input-field col " + tamano}>
-        <select id={id} name={id}>
+        <select value={value} id={id} name={id} onChange={handleChange}>
           <option value="" disabled selected>{label}</option>
             {
                 arr.map((item, index) => 
-                    <option value={index}>{item}</option>
+                    <option key={index} value={item.value}>{item.option}</option>
                 )
             }
         </select>
