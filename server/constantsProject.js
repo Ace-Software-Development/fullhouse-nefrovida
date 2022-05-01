66c2c888643715bb10924c3c27a0ac62ee3e61f1
Andrea Piñeiro <andrea.pineiro.ca@gmail.com>
2022-05-01 14:20:41 -0500

    feat: Agregar constantes de paciente

---
+++ server/constantsProject.js
@@ -0,0 +1,23 @@
+module.exports = Object.freeze({
+    // Parse General
+    OBJECTID: "objectId",
+    IDROL: "idRol",
+    ACTIVO: "activo",
+    // Tabla Colaborador
+    COLABORADOR: "Colaborador",
+    CONTRASENA: "contrasena",
+    IDCOLABORADOR: "idColaborador",
+    NOMBRE: "nombre",
+    APELLIDOPATERNO: "apellidoPaterno",
+    APELLIDOMATERNO: "apellidoMaterno",
+    FECHANACIMIENTO: "fechaNacimiento",
+    SEXO: "sexo",
+    CORREO: "correo",
+    TELEFONO: "telefono",  
+    // Tabla Paciente
+    PACIENTE: "Paciente",
+    CURP: "nombre",
+    ESTATURA: "estatura",
+    PESO: "peso",
+    ACTIVO: "activo"
+})
