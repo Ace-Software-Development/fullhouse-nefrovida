cedfadcc6028648b5f860932ebd748c90d80eab3
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2022-05-05 17:14:36 -0500

    feat: Creación de componente EntradaParametro y vista RegistrarEstudio terminada

--- client/src/Pages/RegistrarEstudio.js
+++ client/src/Pages/RegistrarEstudio.js
@@ -1,19 +1,16 @@
 import { useEffect, useState } from 'react';
 import Main from '../components/Main';
 import Card from '../components/Card';
-import CardLogin from '../components/CardLogin';
 import ContainerForm from '../components/ContainerForm';
 import LineaCampos from '../components/LineaCampos';
 import Input from '../components/Input';
-import BtnIniciarSesion from '../components/BtnIniciarSesion';
-import logo from "../img/logo.png";
 import useLogin from '../hooks/useLogin';
 import validarIniciarSesion from '../util/validators/validarIniciarSesion';
-import BtnRestablecer from '../components/BtnRestablecer';
 import CardTitulo from '../components/CardTitulo';
 import Navbar from '../components/Navbar';
 import BtnRegresar from '../components/BtnRegresar';
 import BtnGuardar from '../components/BtnGuardar';
+import EntradaParametro from '../components/EntradaParametro';
 
 export default function RegistrarEstudio() {
 
@@ -28,7 +25,7 @@ export default function RegistrarEstudio() {
             e.preventDefault()
             
             const response = await fetch('http://localhost:6535/iniciarSesion', { method: 'POST', body: JSON.stringify({
-                "data": formulario}), headers: {'Content-Type': 'application/json'} })
+            "data": formulario}), headers: {'Content-Type': 'application/json'} })
             const iniciarSesion = await response.json()
             console.log(response);
             console.log(iniciarSesion);
@@ -52,51 +49,32 @@ export default function RegistrarEstudio() {
                 <CardTitulo icono="note_add" titulo="Registrar estudio"/>
                     <ContainerForm>
                     <BtnRegresar url="/"/><br/><br/>
-                    <div align="left">               
-                    <div className="detalles-lista negrita-grande c-908F98 left-align">Estudio de Química Sanguínea</div><br/>
-                    <div className="detalles-lista light-pequeno c-908F98 left-align">Estudio para tomar los datos de la química sanguínea de las personas.</div>
-                    </div>
+                    
+                    
+                    
+                        <div align="left">               
+                            <div className="detalles-lista negrita-grande c-908F98 left-align">Estudio de Química Sanguínea  </div><span className='subrayado c-2E7EC8' >  05/05/2022</span><br/>
+                            <div className="detalles-lista light-pequeno c-908F98 left-align">Estudio para tomar los datos de la química sanguínea de las personas.</div>
+                        </div>
+                        <br/>
+                        <div className='identificacion-registrar'/>
+                        <br/>
+
+
                     <form onSubmit={handleSubmit}>
-                        <br></br>
                         <LineaCampos>
-                            <div className="col s7 l6 identificacion-usuario">
-                            <div align="left">
-                            <i className="material-icons icon-separator small c-908F98">format_list_numbered</i><div className="detalles-lista negrita-grande c-908F98 left-align">Sangre:</div><br/>
-                            <Input 
-                                id="username"
-                                type="number" 
-                                label="Valor" 
-                                value={formulario.username}
-                                onChange={handleChange}
-                                tamano="m6 s12"/>
-                                <div className="detalles-lista negrita-pequeno c-908F98 left-align">ml</div>
-                                </div>
-                                </div>
-                                { errors.username 
-                                ? <p></p>
-                                : null}
-                            <div className="col s5 l6">
-                            <div align="left">
-                            <i className="material-icons icon-separator small c-908F98">format_list_numbered</i><div className="detalles-lista negrita-grande c-908F98 left-align">Glucosa:</div><br/>
-                            <Input 
-                                id="password" 
-                                type="number"
-                                label="Valor" 
-                                value={formulario.password}
-                                onChange={handleChange}
-                                tamano="m3 s12"/>
-                                <div className="detalles-lista negrita-pequeno c-908F98 left-align">mg/dL</div>
-                                </div>
-                                </div>
-                                <br></br>
-                                { errors.password
-                                ? <p></p>
-                                : null}
+                            
+                            <EntradaParametro></EntradaParametro>
+                            <EntradaParametro parametro='Glucosa:' unidad='mg/dL'></EntradaParametro>
+                            
                         </LineaCampos>
-                        <br></br>
+                        <div className='identificacion-registrar'/>
+                        <br/>
                         <LineaCampos>
                             <div align="left">
+                            <div className='detalles-usuario'>
                             <i className="material-icons icon-separator small c-908F98">remove_red_eye</i><div className="detalles-lista negrita-grande c-908F98 left-align">Observaciones:</div><br/>
+                            </div>
                             <Input 
                                 id="observaciones" 
                                 label="Ingresa aquí la observación del estudio" 
@@ -104,9 +82,6 @@ export default function RegistrarEstudio() {
                                 onChange={handleChange}
                                 tamano="m12 s12"/>
                                 </div>
-                                { errors.username 
-                                ? <p></p>
-                                : null}
                         </LineaCampos>
                         <br></br>
                         <BtnGuardar/>              
