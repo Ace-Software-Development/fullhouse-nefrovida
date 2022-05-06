6a578d2d5be8152b8698e1d82e51fc775bd43eab
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2022-05-05 20:14:46 -0500

    fix: Cambio de validación con la librería react-hook-form en IniciarSesion

--- client/src/Pages/IniciarSesion.js
+++ client/src/Pages/IniciarSesion.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import M from "materialize-css/dist/js/materialize.min.js";
 import Main from '../components/Main';
 import Card from '../components/Card';
 import CardLogin from '../components/CardLogin';
@@ -7,35 +8,67 @@ import LineaCampos from '../components/LineaCampos';
 import Input from '../components/Input';
 import BtnIniciarSesion from '../components/BtnIniciarSesion';
 import logo from "../img/logo.png";
-import useLogin from '../hooks/useLogin';
-import validarIniciarSesion from '../util/validators/validarIniciarSesion';
 import BtnRestablecer from '../components/BtnRestablecer';
+import { useForm } from 'react-hook-form';
 
-export default function IniciarSesion() {
+const IniciarSesion = () => {
 
-    const [formulario, handleChange, reset, errors] = useLogin({
-        username: '',
-        password: '',
-    }, validarIniciarSesion)
+    const [errorSubmit, setErrorSubmit] = useState("")
+    const [isLoading, setIsLoading] = useState("")
 
-        async function handleSubmit (e) {
+    useEffect(() => {
+        register('username', {
+            required: {
+                value: true,
+                message: "El usuario o correo es requerido"
+            },
+            pattern: {
+                value: /^(?:[A-Z\d][A-Z\d_-]{2,11}|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})$/i,
+                message: "Usuario o Correo inválido"
+            }
+        });
+        register('password', {
+            required: {
+                value: true,
+                message: "La contraseña es requerida"
+            },
+        });
+    }, []);
+
+    const handleChange = (e) => {
+        setValue(e.target.name, e.target.value)
+        setErrorSubmit("")
+    }
+
+    const {register, formState: {errors}, handleSubmit, setValue} = useForm();
+
+
+        async function onSubmit (data, e) {
+            setIsLoading(true)
+            setErrorSubmit("")
 
             e.preventDefault()
-            
-            const response = await fetch('http://localhost:6535/iniciarSesion', { method: 'POST', body: JSON.stringify({
-                "data": formulario}), headers: {'Content-Type': 'application/json'} })
+            try {
+            const response = await fetch('http://localhost:6535/iniciarSesion', { method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'} })
             const iniciarSesion = await response.json()
             console.log(response);
             console.log(iniciarSesion);
             if(!response.ok) {
-                window.alert(iniciarSesion.message);
+                setErrorSubmit(iniciarSesion.message);
                 return;
             }
             else {
-                window.alert(iniciarSesion.message);
+                await M.toast({ html: iniciarSesion.message });
+                window.location.href = "/"
             }
-            console.log(iniciarSesion);
-        }
+        } 
+        catch(e) {
+                setIsLoading(false)
+                setErrorSubmit("Error de conexión. Inténtelo de nuevo.")
+            }
+        };
+
+        console.log("errores", errors)
 
     return(
         <div>
@@ -55,34 +88,53 @@ export default function IniciarSesion() {
                 <Card>
                 <CardLogin titulo="Login"/>
                     <ContainerForm>
-                    <form onSubmit={handleSubmit}>
+
+                    {
+                        isLoading &&
+                        <div class="preloader-wrapper small active">
+                            <div class="spinner-layer spinner-blue-only">
+                            <div class="circle-clipper left">
+                                <div class="circle"></div>
+                            </div><div class="gap-patch">
+                                <div class="circle"></div>
+                            </div><div class="circle-clipper right">
+                                <div class="circle"></div>
+                            </div>
+                            </div>
+                        </div>
+                    }
+                    <br/><br/>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <LineaCampos>
                             <div align="left">
-                            <Input 
+                            <Input
                                 id="username" 
                                 label="Usuario o Correo electrónico" 
-                                value={formulario.username}
-                                onChange={handleChange}
-                                tamano="m12 s12"/>
+                                tamano="m12 s12"
+                                type="text"
+                                onChange={ handleChange }
+                                elError={ errors.username && errors.username?.message }
+                                requerido = { true }/>
                                 </div>
-                                { errors.username 
-                                ? <p>Usuario requerido</p>
-                                : null}
                         </LineaCampos>
                         <LineaCampos>
                             <div align="left">
                             <Input 
                                 id="password" 
-                                type="password"
                                 label="Contraseña" 
-                                value={formulario.password}
-                                onChange={handleChange}
-                                tamano="m12 s12"/>
+                                tamano="m12 s12"
+                                type="password"
+                                onChange = { handleChange }
+                                elError = { errors.password && errors.password?.message }
+                                maxlength = "50"
+                                requerido = { true }/>
                                 </div>
-                                { errors.password
-                                ? <p>Contraseña requerida</p>
-                                : null}
+                                
                         </LineaCampos>
+                        { errorSubmit 
+                            && <div> <div className='red-text right'> <strong> { errorSubmit } </strong> </div> <br/><br/> </div>
+                        }
                         <br></br>
                         <BtnIniciarSesion/>
                         <br></br>
@@ -95,3 +147,5 @@ export default function IniciarSesion() {
         </div>
         )
     }
+
+export default IniciarSesion
