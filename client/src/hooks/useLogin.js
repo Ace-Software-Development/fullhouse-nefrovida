fc33c51303def0874399d5802fbc34048172f713
Felipe <feliyepez2001@gmail.com>
2022-05-13 16:12:49 -0500

    fix: estilo código

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
 const useLogin = (inicial, validate) => {
-    const [formulario, setFormulario] = useState(inicial)
-    const [errors, setErrors] = useState({})
+    const [formulario, setFormulario] = useState(inicial);
+    const [errors, setErrors] = useState({});
 
     const handleChange = (e) => {
         setFormulario({
@@ -10,18 +10,16 @@ const useLogin = (inicial, validate) => {
             [e.target.name]: e.target.type === 'checkbox'
                 ? e.targjnmet.checked
                 : e.target.value
-        })
+        });
 
-        setErrors(validate(formulario))
+        setErrors(validate(formulario));
     }
-
-    console.log(formulario)
 
     const reset = () => {
-        setFormulario(inicial)
+        setFormulario(inicial);
     }
 
-    return [formulario, handleChange, reset, errors]
+    return [formulario, handleChange, reset, errors];
 }
 
 export default useLogin
--- client/src/util/validators/validarIniciarSesion.js
+++ client/src/util/validators/validarIniciarSesion.js
@@ -1,16 +1,13 @@
 export default function validarIniciarSesion(values) {
-    let errors = {}
-    console.log("validar", values.username)
+    let errors = {};
 
     if (!values.username.trim() || values.username == '') {
-        console.log("no usuario")
-        errors.username = "Usuario requerido"
+        errors.username = "Usuario requerido";
     }
 
     if (!values.password.trim() || values.password == '') {
-        console.log("no password")
-        errors.password = "Contraseña requerida"
+        errors.password = "Contraseña requerida";
     }
 
-    return errors
+    return errors;
 }
