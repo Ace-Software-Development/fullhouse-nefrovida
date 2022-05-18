d43ac1387dcd36520b7ca2dfde647f512c5221ce
JARE2001 <A01701350@itesm.mx>
2022-05-17 19:52:23 -0500

    corregir estilo back

--- server/controllers/tipoEstudioController.js
+++ server/controllers/tipoEstudioController.js
@@ -1,11 +1,12 @@
 const tipoEstudio = require('../models/tipoEstudioModel')
 
 module.exports.consultarTipoEstudio = async(request, response) => {
-    let id = request.params.id
-
+    const id = request.params.id;
     try {
-        const result = await tipoEstudio.consultarTipoEstudio(id)
-    } catch (error) {
+        const result = await tipoEstudio.consultarTipoEstudio(id);
+        return result;
+    }
+    catch (error) {
         
     }
 }
