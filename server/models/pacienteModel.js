23751c91cbbc316af439e09723c8b85993b71215
Davidguzley <58478139+Davidguzley@users.noreply.github.com>
2022-05-22 00:08:15 -0500

    ci: clase estudio

--- server/models/pacienteModel.js
+++ server/models/pacienteModel.js
@@ -1,6 +1,38 @@
 const parseServer = require('parse-server').ParseServer;
 let CONSTANTS = require("../constantsProject");
 
+/*
+// Query's que no se si jale
+const paciernte = Parse.Object.extend({});
+//const estudio = Parse.Object.extend({"ESTUDIO"});
+//const tipoEstudio = Parse.Object.extend({"TIPOESTUDIO"});
+
+const queryPaciente = new Parse.Query(paciente);
+//const queryEstudio = new Parse.Query(estudio);
+//const queryTipoEstuio = new Parse.Query(tipoEstudio);
+*/
+
+
+class Parametro{
+    constructor(nombreParametro, unidadParametro, valorResultado, valorReferenciaParametro) {
+        this.nombreParametro = nombreParametro;
+        this.unidadParametro = unidadParametro;
+        this.valorResultado = valorResultado;
+        this.valorReferenciaParametro = valorReferenciaParametro;
+    }
+}
+
+class Estudio{
+    constructor(idPaciente, nombreTipoEstudio, fechaEstudio, descripcionTipoEstudio, parametros, observacionesEstudio) {
+        this.idPaciente = idPaciente;
+        this.nombreTipoEstudio = nombreTipoEstudio;
+        this.fechaEstudio = fechaEstudio;
+        this.descripcionTipoEstudio = descripcionTipoEstudio;
+        this.parametros = parametros;
+        this.observacionesEstudio = observacionesEstudio;
+    }
+}
+
 exports.obtenerEstudioPaciente = async(id) => {
     /*
     var queryObtenerEstudioPaciente = new Parse.Query(CONSTANTS.PACIENTE);
@@ -17,44 +49,45 @@ exports.obtenerEstudioPaciente = async(id) => {
             error: error.message
         }
     }*/
+    const estudioPaciente = new Estudio(id, "Estudio de Química Sanguínea", "05/05/2022", "Estudio para tomar los datos de la química sanguínea de las personas.", [
+        {
+            nombreParametro: "Sangre",
+            unidadParametro: "ml",
+            valorResultado: "59",
+            valorReferenciaParametro: "50 - 60"
+        },
+        {
+            nombreParametro: "Glucosa",
+            unidadParametro: "mg/dL",
+            valorResultado: "25",
+            valorReferenciaParametro: "5 - 20"
+        },
+        {
+            nombreParametro: "Leucositos",
+            unidadParametro: "ml",
+            valorResultado: "100",
+            valorReferenciaParametro: "100 - 150"
+        },
+        {
+            nombreParametro: "Heterositos",
+            unidadParametro: "mg/dL",
+            valorResultado: "28",
+            valorReferenciaParametro: "50 - 100"
+        }
+    ], "Se realizó el estudio 2 veces para verificar los datos obtenidos.");
 
     try {
         //Prueba
-        const estudio = {
-            idPaciente: id,
-            nombreTipoEstudio: "Estudio de Química Sanguínea",
-            fechaEstudio: "05/05/2022",
-            descripcionTipoEstudio: "Estudio para tomar los datos de la química sanguínea de las personas.",
-            parametros: [
-                {
-                    nombreParametro: "Sangre",
-                    unidadParametro: "ml",
-                    valorResultado: "59",
-                    valorReferenciaParametro: "50 - 60"
-                },
-                {
-                    nombreParametro: "Glucosa",
-                    unidadParametro: "mg/dL",
-                    valorResultado: "25",
-                    valorReferenciaParametro: "5 - 20"
-                },
-                {
-                    nombreParametro: "Leucositos",
-                    unidadParametro: "ml",
-                    valorResultado: "100",
-                    valorReferenciaParametro: "100 - 150"
-                },
-                {
-                    nombreParametro: "Heterositos",
-                    unidadParametro: "mg/dL",
-                    valorResultado: "28",
-                    valorReferenciaParametro: "50 - 100"
-                }
-            ],
-            observacionesEstudio: "Se realizó el estudio 2 veces para verificar los datos obtenidos."
+        const result = {
+            idPaciente: estudioPaciente.id,
+            nombreTipoEstudio: estudioPaciente.nombreTipoEstudio,
+            fechaEstudio: estudioPaciente.fechaEstudio,
+            descripcionTipoEstudio: estudioPaciente.descripcionTipoEstudio,
+            parametros: estudioPaciente.parametros,
+            observacionesEstudio: estudioPaciente.observacionesEstudio
         };
         return {
-            estudio: estudio,
+            estudio: result,
             error: null
         }
     } catch (error) {
