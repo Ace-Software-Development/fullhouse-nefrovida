0e3df647ec2bdc39105a9a473f7dfeacfc93d042
JARE2001 <A01701350@itesm.mx>
2022-05-23 21:57:53 -0500

    fix: cambiar el metodo de render al mas actual

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(
   //Datos de cuenta de Auth0
   <Auth0Provider
     domain="dev-gpna0ck9.us.auth0.com"
@@ -12,8 +14,7 @@ ReactDOM.render(
     redirectUri={window.location.origin}
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById("root")
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
