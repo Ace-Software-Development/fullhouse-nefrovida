ff26f9e1a3f31f90da416c26c28f5eb7c4901a9d
JARE2001 <A01701350@itesm.mx>
2022-05-26 15:27:07 -0500

    fix: protect routes import

--- server/routes/tipoEstudioRouter.js
+++ server/routes/tipoEstudioRouter.js
@@ -3,6 +3,8 @@ let CONSTANTS = require('../constantsProject');
 const router = express.Router()
 const tipoEstudio = require('../controllers/tipoEstudioController')
 
+const {authUsuario, noAuthUsuario, authRol} = require('../rbac/Authentication')
+
 
 // ruta get para consutar todos los tipos de estudio
 router.get('/',authRol([CONSTANTS.ROLADMIN, CONSTANTS.ROLQUIMICO]), tipoEstudio.consultarTiposEstudio);
