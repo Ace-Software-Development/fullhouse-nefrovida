5e697a0922b98965ead38f4f9af91a9f17859332
Micho Calzada <A01706202@itesm.mx>
2022-06-06 17:01:47 -0500

    Feature: Agregar tabla admins

--- client/src/Pages/ConsultarColaborador.js
+++ client/src/Pages/ConsultarColaborador.js
@@ -73,6 +73,32 @@ export default function ConsultarColaborador() {
                 }
                 </div>
 
+                <CardSubtitulo subtitulo= "Administradores">
+                </CardSubtitulo>
+                { loading ?  (
+                <div className="center animate-new-element">
+                    <br/>
+                        <div className="preloader-wrapper med active">
+                            <div className="spinner-layer spinner-blue-only">
+                            <div className="circle-clipper left">
+                                <div className="circle"></div>
+                            </div><div className="gap-patch">
+                                <div className="circle"></div>
+                            </div><div className="circle-clipper right">
+                                <div className="circle"></div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <br/>
+                        <br/>
+                    </div>
+                    )
+                : <TablaColaboradores datos= { colaboradores } nombreRol= "admin"/>}
+                { error 
+                    && <div> <div className="red-text center"> <strong> { error } </strong> </div> <br/><br/> </div>
+                }
+
                 <CardSubtitulo subtitulo= "Doctores">
                 </CardSubtitulo>
                 { loading ?  (
