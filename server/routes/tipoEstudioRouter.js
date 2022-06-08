24cbffdd3382532487041cd675c30d12c00113e7
Davidguzley <58478139+Davidguzley@users.noreply.github.com>
2022-06-07 22:29:36 -0500

    feature: Eliminar tipo de estudio

--- server/routes/tipoEstudioRouter.js
+++ server/routes/tipoEstudioRouter.js
@@ -8,6 +8,11 @@ const {authUsuario, noAuthUsuario, authRol} = require('../rbac/Authentication')
 // ruta get para consutar todos los tipos de estudio
 router.get('/',authRol([CONSTANTS.ROLADMIN, CONSTANTS.ROLQUIMICO]), tipoEstudio.consultarTiposEstudio);
 
+// Ruta de tipo 'POST' para eliminar tipo de estudio
+router.post('/id/borrar', 
+    authRol([CONSTANTS.ROLADMIN]),
+    tipoEstudio.eliminarTipoEstudio);
+
 // ruta get para consutar los detalles y parametros de un tipo de estudio
 router.get('/id', authRol([CONSTANTS.ROLADMIN, CONSTANTS.ROLQUIMICO]), tipoEstudio.consultarTipoEstudio);
 
