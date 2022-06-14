2f1cc5b462f8c66e8731577b8869a5583c0bf49c
Manolo Ramírez Pintor <A01706155@itesm.mx>
2022-06-13 23:00:53 -0500

    feat: Mostrar usuario en la navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import logo from "../img/logo.png";
 import CerrarSesion from "./CerrarSesion"
+import { Link } from "react-router-dom";
+import { ReactSession } from 'react-client-session';
 
 
 const Navbar = ({children}) => {
@@ -8,25 +10,31 @@ const Navbar = ({children}) => {
     <div className="navbar-fixed"  >
       <nav className="z-depth-2"  >
         <div className="nav-wrapper c-F9F9F9-transparente"  >
-          <a 
-            href="#!"
-            className="brand-logo"
-          >
-            <img 
-              className="logotipo" 
-              src={ logo }
-              alr="Logotipo Nefrovida"/>
-          </a>
-          <a 
-            href="#"
-            data-target="mobile-demo"
-            className="sidenav-trigger black-text"
-          >
-          <i className="material-icons"  >menu</i>
-          </a>
+          <Link to ={"/"}>
+            <a 
+            className="brand-logo"  
+            >
+              <img 
+                className="logotipo" 
+                src={ logo }
+                alr="Logotipo Nefrovida"/>
+            </a>
+          </Link>
+
           <ul className="right"  >            
             <CerrarSesion/>
           </ul>
+          
+          <ul className="header-flex right c-908F98 hide-usuario">
+            <i class="medium material-icons icon-separator icono-empleado">account_circle</i> 
+            <div>{ ReactSession.get('nombre') + " " + ReactSession.get('apellido') } &nbsp;&nbsp;&nbsp;&nbsp;</div>
+          </ul>
+
+          <ul className="header-flex left c-908F98 show-usuario hide-usuario-new">
+            &nbsp;&nbsp;&nbsp;&nbsp; <i class="medium material-icons icon-separator icono-empleado">account_circle</i> 
+            <div>{ ReactSession.get('nombre') + " " + ReactSession.get('apellido') } </div>
+          </ul>
+          
         </div>
       </nav>
     </div>
