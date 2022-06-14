c1a02af50590afc2b52e43cf2b0f8c980760eafa
JARE2001 <A01701350@itesm.mx>
2022-06-14 10:24:12 -0500

    fix: cambiar espacio entre filas pdf

--- client/src/components/FilaTablaEstudioPDF.jsx
+++ client/src/components/FilaTablaEstudioPDF.jsx
@@ -1,9 +1,9 @@
 const FilaTablaEstudioPDF = ({ estudio }) => {
     return(
         <tr>
-            <td>{ estudio.nombreParametro }</td>
-            <td>{ estudio.valorResultado } { estudio.unidadParametro }</td>
-            <td>{ estudio.valorReferenciaParametro } { estudio.unidadParametro }</td>
+            <td className="pdfTd">{ estudio.nombreParametro }</td>
+            <td className="pdfTd">{ estudio.valorResultado } { estudio.unidadParametro }</td>
+            <td className="pdfTd">{ estudio.valorReferenciaParametro } { estudio.unidadParametro }</td>
         </tr>
     )
 }
